f4bed7276e4c0a9d0fa197b210e26ba7cc33880c
rick-eth0 <endrickmicael29@gmail.com>
2020-12-19 12:14:33 -0300

    ex12

--- exercicio-moeda/teste.py
+++ exercicio-moeda/teste.py
@@ -1,4 +1,5 @@
 from ex111.utilidadesvcev import  moeda
+from  ex112.utilidadesvcev import dado
 
-p = float(input('Digite o preço: R$'))
+p = dado.leiaDinheiro('Digite o preço: R$')
 moeda.resumo(p, 35, 20)
